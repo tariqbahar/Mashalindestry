a33de27a8ece26188b25a749af5644229de78572
Tariq Bahar <97286291+tariqbahar@users.noreply.github.com>
2023-12-25 13:24:16 -0800

    log in update
    add multi type access to user or admin also agent page

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
-use App\Http\Models\user;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class LoginRequest extends FormRequest
 {
@@ -41,14 +42,19 @@ class LoginRequest extends FormRequest
     public function authenticate(): void
     {
         $this->ensureIsNotRateLimited();
+        $user= User::where('email',$this->login)
+                        ->orWhere('name',$this->login)
+                        ->orWhere('phone',$this->login) 
+                        ->first();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (! $user || !Hash::check($this->password,$user->password)) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'login' => trans('auth.failed'),
             ]);
         }
+        Auth::login($user,$this->boolean('remember'));
 
         RateLimiter::clear($this->throttleKey());
     }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,19 +7,20 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="login" :value="__('login')" />
+            <x-text-input id="login" class="block mt-1 w-full" type="text" name="login" :value="old('login')" required autofocus autocomplete="username" />
+           
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="login" :value="__('login')" />
+            <x-input-label for="password" :value="__('password')" />
 
-            <x-text-input id="login" class="block mt-1 w-full"
-                            type="text"
-                            name="login"
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
                             required autocomplete="current-password" />
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
 
            
         </div>
