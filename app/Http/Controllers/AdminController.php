41eadfd6fcb93fe94ab355e843f23e567fa64501
Tariq Bahar <97286291+tariqbahar@users.noreply.github.com>
2023-12-25 16:16:49 -0800

    create admin login
    to access admin in separate log in page admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
 
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     } //end function
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    }//End method
+    public function AdminLogin (){
+        return view('admin.admin_login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth','role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 
 });//end Group Admin Middleware
 
@@ -42,3 +43,4 @@ Route::middleware(['auth','role:agent' ])->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });
 
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
